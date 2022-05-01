b41438cb41fe5848535e862d741eda80b70661e3
deepak <deepakkumardash313@gmail.com>
2022-05-01 21:25:45 +0530

    add : deps & initializing configs

---
+++ storefront/__init__.py
@@ -0,0 +1,9 @@
+import imp
+from flask import Flask
+from storefront.config import configurations
+
+
+def create_app(environment_name='dev'):
+    app = Flask(__name__)
+    app.config.from_object(configurations[environment_name])
+    return app
